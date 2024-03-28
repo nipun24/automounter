e00b459db501f05e7b16116d268e5445cc1dbb78
Nipun Haldar <nipun@apollo.local>
2024-03-28 22:22:38 +0530

    added sleep

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import urllib.request
 import os
 from datetime import datetime
 from env import *
+from time import sleep
 
 if __name__ == "__main__":
     print(datetime.now())
@@ -13,10 +14,18 @@ if __name__ == "__main__":
         exit_code = os.system(f'/sbin/ping -c 1 {res.hostname}')
         if exit_code == 0:
             mount_path = BASE_PATH + res.path
+            # check if path exists
             if os.path.exists(mount_path):
-                print(f'{mount_path} already mounted')
+                # check if path is writable
+                if os.access(mount_path, os.W_OK):
+                    print(f'{mount_path} already mounted')
+                else:
+                    print("NOT WRITABLE!!!")
+                    urllib.request.urlopen(HEALTHCHECK_URL + 'fail',
+                                timeout=10)
             else:
                 os.system(f'open {i}')
+                sleep(10)
                 print(f'mounted {mount_path}')
         else:
             print(exit_code)
